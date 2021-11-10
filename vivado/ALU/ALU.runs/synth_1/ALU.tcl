3ec260515d07a022efc5c19cc3d8d21405a4c949
Enrique Robles Sanz <enrique.robles.sanz@alumnos.upm.es>
2021-11-10 15:46:50 +0100

    Cleanup

--- vivado/ALU/ALU.runs/synth_1/ALU.tcl
+++ vivado/ALU/ALU.runs/synth_1/ALU.tcl
@@ -22,15 +22,15 @@ create_project -in_memory -part xc7a100tcsg324-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/nccas/VivadoWorkspace/ALU/ALU.cache/wt [current_project]
-set_property parent.project_path C:/Users/nccas/VivadoWorkspace/ALU/ALU.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/nccas/VivadoWorkspace/gba/vivado/ALU/ALU.cache/wt [current_project]
+set_property parent.project_path C:/Users/nccas/VivadoWorkspace/gba/vivado/ALU/ALU.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/nccas/VivadoWorkspace/ALU/ALU.cache/ip [current_project]
+set_property ip_output_repo c:/Users/nccas/VivadoWorkspace/gba/vivado/ALU/ALU.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_vhdl -library xil_defaultlib {
-  C:/Users/nccas/VivadoWorkspace/ALU/ALU.srcs/sources_1/new/z80.vhd
-  C:/Users/nccas/VivadoWorkspace/ALU/ALU.srcs/sources_1/new/ALU.vhd
+  C:/Users/nccas/VivadoWorkspace/gba/src/z80.vhd
+  C:/Users/nccas/VivadoWorkspace/gba/src/ALU.vhd
 }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
--- vivado/ALU/ALU.runs/synth_1/runme.sh
+++ vivado/ALU/ALU.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/nccas/VivadoWorkspace/ALU/ALU.runs/synth_1'
+HD_PWD='C:/Users/nccas/VivadoWorkspace/gba/vivado/ALU/ALU.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
